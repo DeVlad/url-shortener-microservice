811dca00ed3768fa2e63a842b9e55cbc090fe875
Deyan Vladkov <deian.vladkov@gmail.com>
2017-09-02 04:37:41 +0100

    Added base36 to short id generator

--- controllers/controller.js
+++ controllers/controller.js
@@ -7,8 +7,7 @@ exports.postUrl = function (req, res) {
         res.send('{ "error": "Invalid URL" }');
     } else {
         var data = new Url({
-            originalUrl: req.query.url,
-            shortUrl: "test"
+            originalUrl: req.query.url            
         });
 
         Url.create(data, function (err, record) {
@@ -23,12 +22,15 @@ exports.postUrl = function (req, res) {
     }
 };
 
-// return base36 to client from incremental id;
-var generateShortUrl = function (sid) {
-    // convert sid to base36 string
+// convert sid to base36 string
+var generateShortUrl = function (sid) {    
     var id = Number(sid);
     return id.toString(36);
 };
+
+var generateSid = function (base36) {
+    return parseInt(base36, 36).toString();
+}
 
 var validateUrl = function (url) {
     var re = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
